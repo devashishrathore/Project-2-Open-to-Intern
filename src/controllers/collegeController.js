78b93a32669277b31589cba51f06b8fefca157b0
jaGdish7898 <93571870+jaGdish7898@users.noreply.github.com>
2021-12-04 15:09:12 +0530

    cola

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -6,14 +6,18 @@ const jwt = require('jsonwebtoken')
 //Q1
 const createCollege = async function (req, res) {
     try {
-        if(!(Object.values(req.body))===0){
+        
+        if(!(Object.values(req.body).length===0)){
             
             let savedCollege = await collegeModel.create(req.body)
             res.status(200).send({ status: true, data: savedCollege })
         }else{
+            console.log(body)
             res.status(400).send({ status: false, msg: "body is empty" })
         }
     } catch (err) {
+        console.log(err)
+        
         res.status(500).send({ status: false, msg: err.message })
     }
 }
